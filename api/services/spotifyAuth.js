c7939a9ed9af21a4b7766f7b2ec25c99df192df9
julicheruse <julicheruse@gmail.com>
2021-02-04 12:06:21 -0300

    functioning search route

--- api/services/spotifyAuth.js
+++ api/services/spotifyAuth.js
@@ -25,7 +25,6 @@ const getAuth = async () => {
       qs.stringify(data),
       headers
     );
-    console.log(response.data.access_token);
     return response.data.access_token;
   } catch (error) {
     console.log(error);
