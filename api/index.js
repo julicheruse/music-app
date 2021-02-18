8af4d8a31bde73ff5772c9af0cfec370c9ab2691
julicheruse <julicheruse@gmail.com>
2021-02-18 11:57:04 -0300

    about router

--- api/index.js
+++ api/index.js
@@ -1,15 +1,15 @@
 const express = require("express");
-const axios = require("axios");
 const cors = require("cors");
 const server = express();
 const searchRouter = require("./routes/search");
-//const searchRouter = require("./src/routes/search.js");
+const aboutRouter = require("./routes/about.js");
 
 server.use(express.urlencoded({ extended: false }));
 server.use(express.json());
 server.use(cors());
 
 server.use("/search", searchRouter);
+server.use("/about", aboutRouter);
 
 server.listen(8888, () => {
   console.log(`Server on port 8888`);
