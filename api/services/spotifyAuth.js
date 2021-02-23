2df686e0915892032e49cdf175db75ac900e4a06
julicheruse <julicheruse@gmail.com>
2021-02-23 12:03:35 -0300

    deleted comment

--- api/services/spotifyAuth.js
+++ api/services/spotifyAuth.js
@@ -3,8 +3,8 @@ const axios = require("axios");
 const qs = require("qs");
 
 const getAuth = async () => {
-  const clientId = process.env.CLIENT_ID; //"3efbd747662a4b2fbaf70456f529eb09";
-  const clientSecret = process.env.CLIENT_SECRET; //"67c16de5b0b54807a2427c2860851909";
+  const clientId = process.env.CLIENT_ID;
+  const clientSecret = process.env.CLIENT_SECRET;
 
   const headers = {
     headers: {
